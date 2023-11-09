27fdd854c07c7c5afd7848e111a090ac4afffb1b
DMYTRO DOLHII <dolgiy.dmitriyp@gmail.com>
2023-11-09 14:26:18 +0100

    Pathfinder 1.0.9

--- PATHFINDER/src/output.c
+++ PATHFINDER/src/output.c
@@ -1,5 +1,49 @@
 #include "../inc/pathfinder.h"
 
+static void floyd_warshall_path(Graph *graph, int *route, int len) {
+    int num_vertices = graph->num_vertices;
+
+    // Initialize the path matrix with the initial route
+    int path[num_vertices][num_vertices];
+    for (int i = 0; i < num_vertices; ++i) {
+        for (int j = 0; j < num_vertices; ++j) {
+            path[i][j] = -1;  // Indicates no intermediate vertex
+        }
+    }
+
+    // Set the initial path matrix based on the given route
+    for (int i = 0; i < len - 1; ++i) {
+        path[route[i]][route[i + 1]] = route[i];
+    }
+
+    // Floyd-Warshall algorithm to find shortest paths and intermediate vertices
+    for (int k = 0; k < num_vertices; ++k) {
+        for (int i = 0; i < num_vertices; ++i) {
+            for (int j = 0; j < num_vertices; ++j) {
+                if (graph->adj_matrix[i][k] != 0 && graph->adj_matrix[k][j] != 0) {
+                    int new_dist = graph->adj_matrix[i][k] + graph->adj_matrix[k][j];
+                    if (graph->adj_matrix[i][j] == 0 || new_dist < graph->adj_matrix[i][j]) {
+                        graph->adj_matrix[i][j] = new_dist;
+                        path[i][j] = k;
+                    }
+                }
+            }
+        }
+    }
+
+    // Reconstruct the intermediate vertices for the given route
+    int start = route[0];
+    int end = route[len - 1];
+    int intermediate = path[start][end];
+    int count = 1;
+
+    while (intermediate != -1) {
+        route[len - count] = intermediate;
+        intermediate = path[start][intermediate];
+        ++count;
+    }
+}
+
 static void print_path_route(Island *islands, int *route, int len) {
     mx_printstr("Path: ");
     mx_printstr(islands[route[0]].name);
