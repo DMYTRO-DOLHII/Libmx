859fcfcfb3f3e10ca68bcc45f2f90c42b677633e
DMYTRO DOLHII <dolgiy.dmitriyp@gmail.com>
2023-11-10 11:45:40 +0100

    Pathfinder 1.1.0

--- PATHFINDER/src/errors.c
+++ PATHFINDER/src/errors.c
@@ -24,18 +24,37 @@ static void file_not_exist_error(char *argv[]) {
 }
 
 static void empty_file_error(char *argv[]) {
-    char *file_content = mx_file_to_str(argv[1]);
+    int descriptor = open(argv[1], O_RDONLY);
 
-    if (file_content[0] == '\0') {
-        mx_printstr("error: file ");
-        mx_printstr(argv[1]);
-        mx_printstr(" is empty\n");
+	char buffer[1];
+	int bytes_read = read(descriptor, buffer, sizeof(buffer));
+
+    if (bytes_read <= 0) {
+        mx_printerr("error: file ");
+        mx_printerr(argv[1]);
+        mx_printerr(" is empty\n");
+        exit(1);
+    }
+
+    close(descriptor);
+}
+
+static void invalid_first_line_error(char *argv[]) {
+    char *file_content = mx_file_to_str(argv[1]);
+    char *first_line = mx_strtok(file_content, "\n");
+
+    if (!mx_isdigit_str(first_line) || mx_atoi(first_line) <= 0) {
+        mx_printstr("error: line 1 is not valid\n");
 
         free(file_content);
         exit(EXIT_FAILURE);
     }
 
     free(file_content);
+}
+
+static void ivalid_line_error(char *argv[]) {
+	
 }
 
 
