fc6499fb6d63ead3fa8bd52da63a46f8b8d4c5f2
DMYTRO DOLHII <dolgiy.dmitriyp@gmail.com>
2023-11-12 11:17:58 +0100

    Makefile

--- ULS/inc/uls.h
+++ ULS/inc/uls.h
@@ -3,7 +3,19 @@
 
 #include "../libmx/inc/libmx.h"
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/acl.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+#include <time.h>
+#include <string.h>
+#include <pwd.h>
+#include <grp.h>
 
 
 
